1225409276b0beebf282625f37a41d614c041fa5
marcedelgado <madelgado1@udc.edu.ar>
2024-09-06 19:39:28 -0300

    creacion de urls

--- TableroMensajes/mensajes/models.py
+++ TableroMensajes/mensajes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+
 # Create your models here.
+Class Mensaje (models.Model):
+    CrearMensajes = models.TextField()
+    MensajesRecibidos = models.TextField()
+  
+    
+    
---
+++ TableroMensajes/mensajes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+#despues importas las vistas .views import ......
+
+app_name = 'mensajes'
+
+urlpatterns = [
+
+]    #aca van los paths
